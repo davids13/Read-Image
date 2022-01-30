478ffa1d179360e43db64efde71bd1a1fd659e2c
David Aniceto <David.Aniceto@ctw.bmwgroup.com>
2022-01-30 16:00:08 +0000

    add new image
    add functionality to read image bytes into an array
    
    delete old image

--- src/com/github/davids13/Main.java
+++ src/com/github/davids13/Main.java
@@ -3,25 +3,35 @@ package com.github.davids13;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        Path imagePathSrc = Path.of("image/free-jpg.jpeg");
+
+        System.out.println("The exits? " + Files.exists(imagePathSrc));
+
         /* read bytes from an image */
-
-        Path imagePathSrc = Path.of("image/spider-man.png");
-
         try (InputStream in = new FileInputStream(imagePathSrc.toString())) {
-
             byte[] allBytes = in.readAllBytes();
-
             for (byte allByte : allBytes) {
                 System.out.printf("0x%x ", +allByte);
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        /* read image bytes to Array of Bytes */
+        byte[] bytesFromImage;
+        try {
+            bytesFromImage = Files.readAllBytes(imagePathSrc);
+            System.out.println("\nArray of bytes" + Arrays.toString(bytesFromImage));
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 }
